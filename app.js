9bacde46b279d795a20e78dae1373a93607d3fe5
Emmanuel Sackey <esackey15@alustudent.com>
2019-08-27 18:05:19 +0400

    Before removing duplicate code

--- app.js
+++ app.js
@@ -21,10 +21,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.urlencoded());
 app.use(express.json());
 
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
-const $ = (require('jquery'))(dom.window);
+let json; // Variable to hold the json data from RDAP
 
 app.get('/', function (req, res) {
     res.render('index.html');
@@ -35,41 +32,92 @@ app.post('/', function (req, res) {
     let entityType = req.body['object-type'];
     let baseUrl = 'https://rdap.afrinic.net/rdap/';
 
+    function commonData() {
+       return {
+        "_comment": "This is the AFRINIC RDAP server.",
+        "header": {
+            "title" : {
+                "language": json.lang,
+                "undefined" : json["notices"][0]["links"][0].value
+            },
+            "description" : {
+                "undefined" : json["notices"][0]["links"][0].href
+            },
+            "baseIris": [
+                "https://rdap.afrinic.net/rdap/",
+            ],
+            "prefixList": {
+                "owl": "http://www.w3.org/2002/07/owl#",
+                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
+                "xsd": "http://www.w3.org/2001/XMLSchema#",
+                "": "http://visualdataweb.de/ontobench/ontology/13/",
+                "xml": "http://www.w3.org/XML/1998/namespace",
+                "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
+            },
+            "iri": "https://rdap.afrinic.net/rdap/"
+        }
+      };
+
+    }
+
+    function getCommonClassAttributes(json, vizJson) {
+        for (let i = 0; i < json.entities.length; i++) {
+            vizJson.class.push({
+                "id": (i + 1).toString(),
+                "type": "owl:Class"
+            });
+            vizJson.classAttribute.push({
+                "id": (i + 1).toString(),
+                "label": json["entities"][i]["handle"],
+                "annotations": {
+                    "language": [{
+                        "identifier": "Language",
+                        "language": json["entities"][i]["lang"],
+                        "value": json["entities"][i].lang,
+                        "type": "label"
+                    }],
+                    "status": [{
+                        "identifier": "Status",
+                        "language": json["entities"][i].lang,
+                        "value": json["entities"][i].status,
+                        "type": "label"
+                    }],
+                    "roles": [{
+                        "identifier": "Roles",
+                        "language": json["entities"][i].lang,
+                        "value": json["entities"][i]["roles"],
+                        "type": "label"
+                    }]
+                },
+            });
+
+            vizJson.property.push({
+                "id": ((i + 1) * 10).toString(),
+                "type": "owl:ObjectProperty"
+            });
+            vizJson.propertyAttribute.push({
+                "id": ((i + 1) * 10).toString(),
+                "label": "entity",
+                "domain": (i + 1).toString(),
+                "range": (0).toString()
+            });
+
+            return vizJson;
+
+    }
+
 
     switch (entityType) {
         case "ip":
             axios.get(baseUrl + 'ip/' + query)
                 .then(response => {
                     let json = response.data;
-                    console.log(json);
-                    let vizJson = {
-                        "_comment": "This is the AFRINIC RDAP server.",
-                        "header": {
-                            "title" : {
-                                "language": json.lang,
-                                "undefined" : json["notices"][0]["links"][0].value
-                            },
-                            "description" : {
-                                "undefined" : json["notices"][0]["links"][0].href
-                            },
-                            "baseIris": [
-                                "https://rdap.afrinic.net/rdap/ip/",
-                            ],
-                            "prefixList": {
-                                "owl": "http://www.w3.org/2002/07/owl#",
-                                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                                "xsd": "http://www.w3.org/2001/XMLSchema#",
-                                "": "http://visualdataweb.de/ontobench/ontology/13/",
-                                "xml": "http://www.w3.org/XML/1998/namespace",
-                                "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-                            },
-                            "iri": "https://rdap.afrinic.net/rdap/ip/"
-                        }
-                    };
+                   // console.log(json);
+                    let vizJson = commonData();
                     vizJson.class = [
                         {
                             "id": "0",
-                            "type": "owl:Class"
+                            "type": "rdfs:Class"
                         }
                     ];
                     vizJson.classAttribute = [
@@ -186,35 +234,12 @@ app.post('/', function (req, res) {
             axios.get(baseUrl + 'autnum/' + query)
                 .then(response => {
                     let json = response.data;
-                    let vizJson = {
-                        "_comment": "This is the AFRINIC RDAP server.",
-                        "header": {
-                            "title": {
-                                "language": json.hreflang,
-                                "undefined": json["notices"][0]["links"][0].value
-                            },
-                            "description": {
-                                "undefined": json["notices"][0]["links"][0].href
-                            },
-                            "baseIris": [
-                                "https://rdap.afrinic.net/rdap/autnum/",
-                            ],
-                            "prefixList": {
-                                "owl": "http://www.w3.org/2002/07/owl#",
-                                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                                "xsd": "http://www.w3.org/2001/XMLSchema#",
-                                "": "http://visualdataweb.de/ontobench/ontology/13/",
-                                "xml": "http://www.w3.org/XML/1998/namespace",
-                                "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-                            },
-                            "iri": "https://rdap.afrinic.net/rdap/autnum/"
-                        }
-                    };
+                    let vizJson = commonData();
 
                     vizJson.class = [
                         {
                             "id": "0",
-                            "type": "owl:Class"
+                            "type": "rdfs:Class"
                         }
                     ];
                     vizJson.classAttribute = [
@@ -310,41 +335,13 @@ app.post('/', function (req, res) {
         case "rdns":
             axios.get(baseUrl + 'domain/' + query)
                 .then(response => {
-                    let json = response.data;
+                     json = response.data;
 
-                    let vizJson = {
-                        "_comment": "This is the AFRINIC RDAP server.",
-                        "header": {
-                            "title" : {
-                                "language": json.lang,
-                                "undefined" : json["notices"][0]["links"][0].value
-                            },
-
-                            "description" : {
-                                "undefined" : json["notices"][0]["links"][0].href
-                            },
-
-                            "baseIris": [
-                                "https://rdap.afrinic.net/rdap/domain/",
-                            ],
-
-                            "prefixList": {
-                                "owl": "http://www.w3.org/2002/07/owl#",
-                                "rdf": "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
-                                "xsd": "http://www.w3.org/2001/XMLSchema#",
-                                "": "http://visualdataweb.de/ontobench/ontology/13/",
-                                "xml": "http://www.w3.org/XML/1998/namespace",
-                                "rdfs": "http://www.w3.org/2000/01/rdf-schema#"
-                            },
-
-                            "iri": "https://rdap.afrinic.net/rdap/domain/"
-                        }
-                    };
-
+                    let vizJson = commonData();
                     vizJson.class = [
                         {
                             "id": "0",
-                            "type": "owl:Class"
+                            "type": "rdfs:Class"
                         }
                     ];
                     vizJson.classAttribute = [
@@ -469,10 +466,7 @@ app.post('/', function (req, res) {
                         };
 
                         vizJson.propertyAttribute.push(_propertyAttribute);
-
-
                     }
-
                     saveFile(vizJson);
 
                 } ).catch(function (err) {
@@ -480,8 +474,92 @@ app.post('/', function (req, res) {
             });
             break;
 
-
         case "entity":
+            axios.get(baseUrl + 'entity/' + query).
+            then(response => {
+              json = response.data;
+              let vizJson = commonData();
+                vizJson.class = [
+                    {
+                        "id": "0",
+                        "type": "rdfs:Class"
+                    }
+                ];
+
+                vizJson.classAttribute = [
+                    {
+                        "id": "0",
+                        "label": json.handle,
+                        "comment": {
+                            "undefined": json["objectClassName"]
+                        },
+
+                        "annotations": {
+                            "handle": [{
+                                "identifier": "Handle",
+                                "language": json.lang,
+                                "value": json.handle,
+                                "type": "label"
+                            }],
+
+                            "status": [{
+                                "identifier": "Status",
+                                "language": json.lang,
+                                "value": json.status,
+                                "type": "label"
+                            }]
+                        }
+                    }
+                ];
+
+                for(let i = 0; i < json.entities.length; i++){
+                    vizJson.class.push({
+                        "id": (i + 1).toString(),
+                        "type": "owl:Class"
+                    });
+
+                    let _classAttribute = {
+                        "id": (i + 1).toString(),
+                        "label": json["entities"][i]["handle"],
+                        "annotations": {
+
+                            "language": [{
+                                "identifier": "Language",
+                                "language": json["entities"][i]["lang"],
+                                "value": json["entities"][i].lang,
+                                "type": "label"
+                            }],
+
+                            "status": [{
+                                "identifier": "Status",
+                                "language": json["entities"][i].lang,
+                                "value": json["entities"][i].status,
+                                "type": "label"
+                            }],
+
+                            "roles": [{
+                                "identifier": "Roles",
+                                "language": json["entities"][i].lang,
+                                "value": json["entities"][i]["roles"],
+                                "type": "label"
+                            }],
+
+                            "Object Type": [{
+                                "identifier": "Object Type",
+                                "language": json["entities"][i].lang,
+                                "value": json["entities"][i]["objectClassName"],
+                                "type": "label"
+                            }]
+                        }
+                    };
+                    vizJson.classAttribute.push(_classAttribute);
+                }
+                saveFile(vizJson);
+
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
             break;
 
     }
